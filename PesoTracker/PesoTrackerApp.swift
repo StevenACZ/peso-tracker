c4be37b7ee1f0a4698223abc58d5f301fd12a516
Oliverio23 <scoaila@proton.me>
2025-07-25 22:39:37 -0500

    Initial Commit

---
+++ PesoTracker/PesoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PesoTrackerApp.swift
+//  PesoTracker
+//
+//  Created by Steven Coaila Zaa on 25/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PesoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
